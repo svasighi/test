e23aac6a58ebce30bf23885d760bc5ddbc66a50b
shadmehr vasighi <shadmehr.vasighi99@gmail.com>
2023-12-23 18:00:16 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -124,6 +124,7 @@ const Home = () => {
                 onClick={fetchData}
               >
                 {isLoading ? (
+                <>
                   <svg
                     class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
                     xmlns="http://www.w3.org/2000/svg"
@@ -140,11 +141,13 @@ const Home = () => {
                     ></circle>
                     <path
                       class="opacity-75"
-                      fill="currentColor"
+                      fill="#141B34"
                       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                     ></path>
                   </svg>
-                ) : (
+                    درحال بارگذاری
+                      </>                
+                  ) : (
                   "بررسی🔎"
                 )}
               </div>
