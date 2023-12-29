79ce558aaace0ba373fc261a9b80274646181fac
shadmher <shadmehr.vasighi99@gmail.com>
2023-12-29 04:09:42 +0330

    new

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,18 @@
-import "./globals.css";
-import { Vazirmatn } from "next/font/google";
+import './globals.css';
+import { Vazirmatn } from 'next/font/google';
 
-const vazirmant = Vazirmatn({ subsets: ["latin"] });
+const vazirmant = Vazirmatn({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "اطلاعات  آیپیم چیه؟",
-  description: "توسط فاطمه ندافی",
+  title: 'اطلاعات  آیپیم چیه؟',
+  description: 'توسط فاطمه ندافی',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" dir="rtl">
       <body
-        className={`${vazirmant.className} bg-blured bg-cover bg-center bg-no-repeat h-full`}
-      >
+        className={`${vazirmant.className} bg-blured bg-cover bg-center bg-no-repeat h-full`}>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,15 +1,23 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import axios from "axios";
-import NeshanMap from "react-neshan-map-leaflet";
-import Image from "next/image";
-import closeIcon from "@/public/close.svg";
+import { useState } from 'react';
+import axios from 'axios';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  CircleMarker,
+} from 'react-leaflet';
+import Image from 'next/image';
+import closeIcon from '@/public/close.svg';
+import 'leaflet/dist/leaflet.css';
 
 const Home = () => {
   const [userInfo, setUserInfo] = useState(null);
   const [dataFetched, setDataFetched] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isFinishedEveryThing, setIsFinishedEveryThing] = useState(false);
 
   const fetchData = async () => {
     try {
@@ -30,16 +38,20 @@ const Home = () => {
         agentInfo: agentResponse?.data,
       });
       setDataFetched(true);
+      setIsFinishedEveryThing(true);
+
+      setTimeout(() => {
+        setIsFinishedEveryThing(false);
+      }, 5000);
     } catch (error) {
       setIsLoading(false);
-      console.error("Error fetching user information:", error);
     }
   };
   return (
-    <div className="container mx-auto my-10">
+    <div className={`container mx-auto py-10 ${dataFetched ? '' : 'h-screen'}`}>
       <Image
         src="./logo.svg"
-        alt={"logo"}
+        alt={'logo'}
         width={266}
         height={62}
         className="mt-24 mr-10"
@@ -47,10 +59,9 @@ const Home = () => {
 
       <div className="flex justify-center items-center h-[calc(100vh-190px)]">
         <div
-          className={`rounded-[34px] md:rounded-[56px] md:w-[620px] w-[320px] bg-white/15 backdrop-blur-md flex flex-col ${
-            dataFetched ? "items-start" : "items-end"
-          } gap-4 md:gap-11 p-4 md:p-9 border border-gradient `}
-        >
+          className={`rounded-[34px] md:rounded-[56px] md:w-[620px] w-[450px] bg-white/15 backdrop-blur-md flex flex-col ${
+            dataFetched ? 'items-start' : 'items-end'
+          } gap-4 md:gap-11 p-4 md:p-9 border border-gradient `}>
           {dataFetched ? (
             <div className="w-full">
               <div className="flex justify-between items-center">
@@ -59,8 +70,7 @@ const Home = () => {
                   className="text-3xl flex justify-center items-center font-bold mb-4 cursor-pointer ml-4 h-16 w-16 bg-primary rounded-full line"
                   onClick={() => {
                     setDataFetched(false);
-                  }}
-                >
+                  }}>
                   <Image src={closeIcon} alt="" />
                 </div>
               </div>
@@ -68,12 +78,24 @@ const Home = () => {
                 <div className="mt-10">
                   <div className="grid grid-cols-2 gap-3 text-xl">
                     <p>آدرس آیپی: {userInfo?.agentInfo?.ipString}</p>
-                    <p>شهر: {userInfo?.ipInfo?.city?.names?.fa}</p>
                     <p>
-                      استان / ایالت:{" "}
-                      {userInfo?.ipInfo?.subdivisions[0].names.fa}
+                      شهر:
+                      {userInfo?.ipInfo?.country.iso_code == 'IR'
+                        ? 'تهران'
+                        : userInfo?.ipInfo?.city?.names?.fa}
                     </p>
-                    <p>کشور: {userInfo?.ipInfo?.country.names.fa}</p>
+                    <p>
+                      استان / ایالت:{' '}
+                      {userInfo?.ipInfo?.country.iso_code == 'IR'
+                        ? 'تهران'
+                        : userInfo?.ipInfo?.subdivisions[0].names.fa}
+                    </p>
+                    <p>
+                      کشور:{' '}
+                      {userInfo?.ipInfo?.country.iso_code == 'IR'
+                        ? 'ایران'
+                        : userInfo?.ipInfo?.country.names.fa}
+                    </p>
                     <p>
                       نوع اتصال:
                       {userInfo?.ipInfo?.traits?.connection_type}
@@ -85,23 +107,69 @@ const Home = () => {
                   </div>
 
                   <div className="bg-primary mt-10 p-3 overflow-hidden rounded-2xl">
-                    {
-                      <NeshanMap
-                        options={{
-                          key: "web.8269ae1efaee442aac942c23eb9df7f7",
-                          maptype: "dreamy",
-                          poi: true,
-                          traffic: false,
-                          center: [
-                            userInfo?.ipInfo?.location?.latitude ?? 35.699739,
-                            userInfo?.ipInfo?.location?.longitude ?? 51.338097,
-                          ],
-                          zoom:
-                            userInfo?.ipInfo?.country.iso_code != "IR" ? 3 : 13,
-                        }}
-                        className="h-[200px] md:h-[450px] rounded-2xl"
-                      />
-                    }
+                    {isFinishedEveryThing ? (
+                      <div className="backdrop-blur-md text-3xl font-bold flex flex-col items-center justify-center h-96 gap-10">
+                        <svg
+                          class="animate-spin h-10 w-10 "
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="transparent"
+                          viewBox="0 0 24 24">
+                          <circle
+                            class="opacity-100 bg-slate-200"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="#2B3C59"
+                            stroke-width="4"></circle>
+                          <path
+                            class="opacity-100"
+                            fill="#FFB802"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        درحال بررسی موقعیت مکانی برحسب IP
+                      </div>
+                    ) : (
+                      <MapContainer
+                        center={[
+                          userInfo?.ipInfo?.country.iso_code == 'IR'
+                            ? 35.702163
+                            : userInfo?.ipInfo?.location?.latitude,
+                          userInfo?.ipInfo?.country.iso_code == 'IR'
+                            ? 51.400671
+                            : userInfo?.ipInfo?.location?.longitude,
+                        ]}
+                        zoom={18}
+                        scrollWheelZoom={false}
+                        className="h-[400px]">
+                        <TileLayer
+                          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <CircleMarker
+                          center={[
+                            userInfo?.ipInfo?.country.iso_code == 'IR'
+                              ? 35.702163
+                              : userInfo?.ipInfo?.location?.latitude,
+                            userInfo?.ipInfo?.country.iso_code == 'IR'
+                              ? 51.400671
+                              : userInfo?.ipInfo?.location?.longitude,
+                          ]}
+                          pathOptions={{ color: 'red' }}
+                          radius={50}>
+                          <Popup>موقعیت مکانی برحسب ip</Popup>
+                        </CircleMarker>
+                        <Marker
+                          // icon={myIcon}
+                          position={[
+                            userInfo?.ipInfo?.country.iso_code == 'IR'
+                              ? 35.702163
+                              : userInfo?.ipInfo?.location?.latitude,
+                            userInfo?.ipInfo?.country.iso_code == 'IR'
+                              ? 51.400671
+                              : userInfo?.ipInfo?.location?.longitude,
+                          ]}></Marker>
+                      </MapContainer>
+                    )}
                   </div>
                 </div>
               ) : (
@@ -110,45 +178,41 @@ const Home = () => {
             </div>
           ) : (
             <>
-              <div className="bg-primary w-full rounded-[20px] md:rounded-[36px]  grid grid-cols-1 gap-y-5 md:gap-y-9 p-4 md:p-10 ">
-                <div className="text-secondary text-lg md:text-3xl">
+              <div className="bg-primary w-full rounded-[20px] md:rounded-[36px]  grid grid-cols-1 gap-y-6 md:gap-y-9 p-10">
+                <div className="text-secondary text-2xl md:text-3xl">
                   👩🏻 توسط فاطمه ندافی
                 </div>
                 <hr className="border-secondary" />
-                <div className="text-secondary text-lg md:text-5xl font-black">
+                <div className="text-secondary text-3xl md:text-5xl font-black">
                   🚀نمایش اطلاعات آیپی
                 </div>
               </div>
               <div
-                className="cursor-pointer px-5 py-3 text-lg  md:px-10 md:py-6 rounded-full md:text-2xl bg-primary text-secondary font-black"
-                onClick={fetchData}
-              >
+                className="cursor-pointer px-7 py-4 text-xl  md:px-10 md:py-6 rounded-full md:text-2xl bg-primary text-secondary font-black"
+                onClick={fetchData}>
                 {isLoading ? (
-                <div className="flex items-center gap-2">
-                  <svg
-                    class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      class="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      stroke-width="4"
-                    ></circle>
-                    <path
-                      class="opacity-75"
-                      fill="#141B34"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
+                  <div className="flex items-center gap-2">
+                    <svg
+                      class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24">
+                      <circle
+                        class="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"></circle>
+                      <path
+                        class="opacity-75"
+                        fill="#141B34"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
                     درحال بارگذاری
-                      </div>                
-                  ) : (
-                  "بررسی🔎"
+                  </div>
+                ) : (
+                  'بررسی🔎'
                 )}
               </div>
             </>
